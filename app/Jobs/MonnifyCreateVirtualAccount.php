4aa0c4c2820a2a56edc47d82bea18eac967103ab
Odejinmi Samuel <odejinmisamuel@gmail.com>
2024-12-08 17:58:03 +0100

    -Fix monnify error

--- app/Jobs/MonnifyCreateVirtualAccount.php
+++ app/Jobs/MonnifyCreateVirtualAccount.php
@@ -39,12 +39,13 @@ class MonnifyCreateVirtualAccount implements ShouldQueue
 
             $fc=FundingConfig::where('name','Monnify')->first();
 
-            $fcv=explode("|",$fc->value);
+            $fcv=explode("|",$fc->ppkey);
 
             $apiKey=$fcv[0];
             $secretKey=$fcv[1];
             $contractCode=$fcv[2];
 
+            Log::info("Create Monnify Account for " . $user->email);
 
             $curl = curl_init();
 
@@ -66,9 +67,11 @@ class MonnifyCreateVirtualAccount implements ShouldQueue
 
             curl_close($curl);
 
+            Log::info("Create Monnify Account Login response " . $respons);
+
 //        $response='{"requestSuccessful":true,"responseMessage":"success","responseCode":"0","responseBody":{"accessToken":"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsibW9ubmlmeS1wYXltZW50LWVuZ2luZSJdLCJzY29wZSI6WyJwcm9maWxlIl0sImV4cCI6MTU5MTQ5Nzc5OSwiYXV0aG9yaXRpZXMiOlsiTVBFX01BTkFHRV9MSU1JVF9QUk9GSUxFIiwiTVBFX1VQREFURV9SRVNFUlZFRF9BQ0NPVU5UIiwiTVBFX0lOSVRJQUxJWkVfUEFZTUVOVCIsIk1QRV9SRVNFUlZFX0FDQ09VTlQiLCJNUEVfQ0FOX1JFVFJJRVZFX1RSQU5TQUNUSU9OIiwiTVBFX1JFVFJJRVZFX1JFU0VSVkVEX0FDQ09VTlQiLCJNUEVfREVMRVRFX1JFU0VSVkVEX0FDQ09VTlQiLCJNUEVfUkVUUklFVkVfUkVTRVJWRURfQUNDT1VOVF9UUkFOU0FDVElPTlMiXSwianRpIjoiOTYyNTA5NzctMmZkOS00ZDM4LTliYzEtNTMyMTMwYmFiODc0IiwiY2xpZW50X2lkIjoiTUtfVEVTVF9LUFoyQjJUQ1hLIn0.iTOX9RWwA0zcLh3OsTtuFD-ehAbW1FrUcAZLM73V66_oTuV2jJ5wBjWNvyQToZKl2Rf5TH2UgiJyaapAZR6yU9Y4Di_oz97kq0CwpoFoe_rLmfgWgh-jrYEsrkj751jiQQm_vZ6BEw9OJhYtMBb1wEXtY4rFMC7I2CLmCnwpJaMWgrWnTRcoLZlPTcWGMBLeggaY9oLfIIorV9OTVkB2kihA9QHX-8oUGkYpvKyC9ERNYMURcK01LnPgSBWI7lXrjf8Ct2BjHi6RKdlFRPNpp3OAbN9Oautvwy09WS3XOhA8eycA0CNBh8o7jekVLCLjXgz6YrcMH0j9ahb3mPBr7Q","expiresIn":368}}';
 
-            $response = json_decode($response, true);
+            $response = json_decode($respons, true);
             $token = $response['responseBody']['accessToken'];
 
             $fn=$user->lastname . " ". $user->firstname;
@@ -84,7 +87,6 @@ class MonnifyCreateVirtualAccount implements ShouldQueue
 	"getAllAvailableBanks": true
 }';
 
-            Log::info("Create Monnify Account for " . $user->email);
             Log::info($payload);
 
             $curl = curl_init();
