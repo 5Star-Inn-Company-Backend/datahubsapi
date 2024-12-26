fe99aac3b141944f95ce92486554b77f76717c65
Odejinmi Samuel <odejinmisamuel@gmail.com>
2024-12-26 15:29:51 +0100

    -Worked on command to updat ename and prices of data weekyly

---
+++ app/Console/Commands/FetchMCDPlansCommand.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\tbl_serverconfig_data;
+use Illuminate\Console\Command;
+
+class FetchMCDPlansCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:mcdplans';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch MCD plans';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->dataPlans();
+    }
+
+    private function dataPlans()
+    {
+        $this->info("Fetching data plans");
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => env('MCD_URL') . "/data/ALL",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . env('MCD_TOKEN'),
+                'Content-Type: application/json'
+            ),
+        ));
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $response = curl_exec($curl);
+
+        echo $response;
+
+        curl_close($curl);
+
+        $reps = json_decode($response, true);
+
+        if($reps['success'] == 1){
+            foreach ($reps['data'] as $rep){
+                if($rep['server'] == 10 || $rep['server'] == 6){
+                    $fp=tbl_serverconfig_data::where('coded',$rep['coded'])->first();
+
+                    if($fp){
+                        $this->updateNewPlan($fp->id,$rep['price'],$rep['name']);
+                    }else{
+                        $this->createNewPlan($rep);
+                    }
+                }
+            }
+        }
+
+    }
+
+    private function createNewPlan($rep){
+        $this->info("Creating plan " . $rep['name']." ".$rep['network']);
+        tbl_serverconfig_data::create([
+            'name' => $rep['name'],
+            'dataplan' => $rep['dataplan'],
+            'category' => $rep['category'],
+            'network' => $rep['network'],
+            'coded' => $rep['coded'],
+            'plan_id' => $rep['coded'],
+            'amount' => $rep['price']+30,
+            'price' => $rep['price'],
+            'server' => $rep['server'],
+            'status' => 1,
+        ]);
+    }
+
+    private function updateNewPlan($id,$price,$name){
+        $this->info("Updating plan $id - $name - $price");
+        tbl_serverconfig_data::where('id',$id)->update([
+            'name' => $name,
+            'amount' => $price+30,
+            'price' => $price
+        ]);
+    }
+
+}
--- app/Models/tbl_serverconfig_data.php
+++ app/Models/tbl_serverconfig_data.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class tbl_serverconfig_data extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
